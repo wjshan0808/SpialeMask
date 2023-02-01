5d241ac2da19175ae64276c4830825f685b154c9
wjshan0808 <wjshan0808@gmail.com>
2023-02-01 19:22:17 +0800

    移动库文件

--- Source/SpialeMask/QmakePostLink.bat
+++ Source/SpialeMask/QmakePostLink.bat
@@ -3,8 +3,8 @@
 ::%1第一个参数值等价与 %~dp0脚本文件所在目录
 ::
 
-::复制库文件到库目录
-XCOPY "%1\..\..\Release\libSpialeMask.a" "%~dp0..\..\Library" /Y
+::移动库文件到库目录
+MOVE /Y "%1\..\..\Release\libSpialeMask.a" "%~dp0..\..\Library"
 
 ::复制头文件到包含目录
 XCOPY "%1\*.h" "%~dp0..\..\Include" /Y
